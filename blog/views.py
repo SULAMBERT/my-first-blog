0f4aa37eb360b859ee9ac72d8974f9acfefc7f5f
SULAMBERT <subhendu.lambert@gmail.com>
2019-01-27 23:56:45 +0530

    the modified

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from .models import Post
 
 def post_list(request):
-	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts=Post.objects.all().order_by('published_date')
 	return render(request, 'blog/post_list.html',{'posts': posts})#posts is django template tags 
 	# return HttpResponse("hey")
 
