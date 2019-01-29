d7635f0a5e1ebffa6c4ad71eff72d192d5763c30
SULAMBERT <subhendu.lambert@gmail.com>
2019-01-29 22:09:06 +0530

    fsf

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':'/Users/curofy/Desktop/myproject/djangogirls/db.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
