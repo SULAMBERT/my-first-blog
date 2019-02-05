92dc25bbd88ee214340e76007b3a6f4091f168fa
SULAMBERT <subhendu.lambert@gmail.com>
2019-02-05 21:57:38 +0530

    Added view and template

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'post/(?P<pk>[0-9])/',views.post_detail, name='post_detail'),
     url('', views.post_list, name='post_list'),
-    # url('post/<int:pk>/',views.post_detail, name='post_detail'),
+
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 
-from django.shortcuts import render, get_object_or_404
+ 
 
 def post_list(request):
 	posts=Post.objects.all()
@@ -14,8 +14,8 @@ def post_list(request):
 
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
 
 
 
